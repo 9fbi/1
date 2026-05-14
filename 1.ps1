bfbf76736ddbae9a4059017de033b9acb4e3a856
9fbi <amankumarfrost@gmail.com>
2026-05-14 13:24:57 +0530

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,4 +1,4 @@
-wt `
-new-tab powershell -NoExit -Command "$Host.UI.RawUI.WindowTitle='ADB Dev'; function prompt { 'PS-ADB> ' }; while (`$true) { adb devices; Start-Sleep 1; Clear-Host }" `
-; new-tab powershell -NoExit -Command "$Host.UI.RawUI.WindowTitle='Fastboot Dev'; function prompt { 'PS-FASTBOOT> ' }; while (`$true) { fastboot devices; Start-Sleep 1; Clear-Host }" `
-; new-tab powershell -NoExit -Command "$Host.UI.RawUI.WindowTitle='ADB Logcat'; function prompt { 'PS-LOGCAT> ' }; while (`$true) { adb devices; Start-Sleep 1; Clear-Host }"
+wt -w 0 `
+nt powershell -NoExit -Command "function prompt { 'PS-ADB> ' }; while (`$true) { adb devices; Start-Sleep 1; Clear-Host }" `
+; nt powershell -NoExit -Command "function prompt { 'PS-FASTBOOT> ' }; while (`$true) { fastboot devices; Start-Sleep 1; Clear-Host }" `
+; nt powershell -NoExit -Command "function prompt { 'PS-LOGCAT> ' }; while (`$true) { adb logcat }"
