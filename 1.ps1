e2cecc3f9b889cffa0b2ba012295d07a607d7fa4
9fbi <amankumarfrost@gmail.com>
2026-05-14 13:22:34 +0530

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,3 +1,4 @@
-Start-Process powershell -ArgumentList "-NoExit","-Command","`$Host.UI.RawUI.WindowTitle='ADB Dev'; function prompt { `":~ `" }; while (`$true) { adb devices; Start-Sleep 1; Clear-Host }"
-Start-Process powershell -ArgumentList "-NoExit","-Command","`$Host.UI.RawUI.WindowTitle='Fastboot Dev'; function prompt { `"Fastboot Dev `" }; while (`$true) { fastboot devices; Start-Sleep 1; Clear-Host }"
-Start-Process powershell -ArgumentList "-NoExit","-Command","`$Host.UI.RawUI.WindowTitle='ADB Logcat'; function prompt { `"ADB Dev `" }; while (`$true) { adb devices; Start-Sleep 1; Clear-Host }"
+wt `
+new-tab powershell -NoExit -Command "$Host.UI.RawUI.WindowTitle='ADB Dev'; function prompt { 'PS-ADB> ' }; while (`$true) { adb devices; Start-Sleep 1; Clear-Host }" `
+; new-tab powershell -NoExit -Command "$Host.UI.RawUI.WindowTitle='Fastboot Dev'; function prompt { 'PS-FASTBOOT> ' }; while (`$true) { fastboot devices; Start-Sleep 1; Clear-Host }" `
+; new-tab powershell -NoExit -Command "$Host.UI.RawUI.WindowTitle='ADB Logcat'; function prompt { 'PS-LOGCAT> ' }; while (`$true) { adb devices; Start-Sleep 1; Clear-Host }"
