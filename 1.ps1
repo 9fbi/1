9cfcac444a3d78ac7232602504aaa22611c8027a
9fbi <amankumarfrost@gmail.com>
2026-08-21 15:32:06 +0530

    Delete 1.ps1

--- 1.ps1
+++
@@ -1,4 +0,0 @@
-wt -w 0 `
-nt powershell -NoExit -Command "function prompt { 'PS-ADB> ' }; while (`$true) { adb devices; Start-Sleep 1; Clear-Host }" `
-; nt powershell -NoExit -Command "function prompt { 'PS-FASTBOOT> ' }; while (`$true) { fastboot devices; Start-Sleep 1; Clear-Host }" `
-; nt powershell -NoExit -Command "function prompt { 'PS-LOGCAT> ' }; while (`$true) { adb logcat }"
